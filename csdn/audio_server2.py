1738ce8102536a19e211c30c0a3be1139e8c4b3f
li_qiu_jieqi <13757505+li-qiu-jieqi@user.noreply.gitee.com>
2024-02-25 18:30:13 +0800

    1.音频更新

--- csdn/audio_server2.py
+++ csdn/audio_server2.py
@@ -1,45 +1,69 @@
+# 导入所需的库
 import asyncio
 import websockets
 import pyaudio
 
-chunk = 4096  # Record in chunks of 1024 samples
-sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 2  # 两声道
-fs = 44100  # Record at 44100 samples per second
-p = pyaudio.PyAudio()  # 创建音频对象
+# 定义音频块的大小，这里是4096字节
+chunk = 4096
 
-# image = cv2.imread("1.png")#image :是返回提取到的图片的值
-# 麦克风
-micstream = p.open(format=sample_format,  # 音频输入流创建
+# 定义音频样本的格式为16位整数
+sample_format = pyaudio.paInt16
+
+# 定义声道数为2，即立体声
+channels = 2
+
+# 定义采样率为44100Hz
+fs = 44100
+
+# 创建一个PyAudio实例，用于音频输入和输出
+p = pyaudio.PyAudio()
+
+# 打开麦克风输入流
+micstream = p.open(format=sample_format,
                    channels=channels,
                    rate=fs,
                    frames_per_buffer=chunk,
                    input=True)
-# 喇叭
-stream2 = p.open(format=sample_format,  # 音频输出流创建
+
+# 打开音频输出流
+stream2 = p.open(format=sample_format,
                  channels=channels,
                  rate=fs,
                  frames_per_buffer=chunk,
                  output=True)
 
 
+# 定义一个异步函数echo，用于处理WebSocket连接
 async def echo(websocket, path):
+    # 打印websockets.serve的信息（但实际上这里应该是websockets.serve()的调用信息）
     print(websockets.serve)
+
+    # 异步迭代WebSocket接收到的消息
     async for message in websocket:
+        # 将接收到的WebSocket消息写入音频输出流（播放音频）
+        print(message)
         stream2.write(message)  # 播放
-        # m=micstream.read(chunk)#录音
+
+        # 初始化一个空列表y，用于存储从麦克风读取的音频数据
         y = []
-        for i in range(1):  # 这里和上一篇数字不一样请注意。15次录音会阻塞线程。需要开新线程处理。范例用1次可忽略阻塞影响。
+
+        # 从麦克风读取一块音频数据（这里只读取一次）
+        for i in range(1):
             m = micstream.read(chunk)
             y.append(m)
 
-        if len(message) > 1: await websocket.send(y)  # 发送录音
+            # 如果WebSocket消息的长度大于1，则将其（以及从麦克风读取的数据）发送回WebSocket客户端
+        if len(message) > 1: await websocket.send(y)
+
+    # 定义一个异步函数main，用于启动WebSocket服务器
 
 
 async def main():
-    # start a websocket server
-    async with websockets.serve(echo, "172.20.41.114", 8765):
+    # 使用websockets库启动WebSocket服务器，监听指定IP和端口，并处理连接使用echo函数
+    async with websockets.serve(echo, "192.168.60.105", 8765):
+        # 创建一个未完成的Future对象，使得服务器持续运行，直到被外部因素（如Ctrl+C）中断
         await asyncio.Future()  # run forever
 
 
+# 使用asyncio库运行main函数，启动WebSocket服务器
 asyncio.run(main())
